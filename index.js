bcb6a1fff71a707b685c259bd13ed02e3c52e786
Dean <mccarthey123@outlook.com>
2018-05-22 17:58:22 +0800

    自动化UI测试

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const puppeteer = require('puppeteer');
-const devices = require('puppeteer/DeviceDescriptors');
+const puppeteer = require("puppeteer")
+const devices = require("puppeteer/DeviceDescriptors")
 
-(async ()=>{
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.emulate(devices['iPhone 6'])
-    await page.goto('https://www.baidu.com');
-    await page.screenshot({path:'example.png',fullPage:true});
-    await page.pdf({path: 'test.pdf', format: 'A4'});
-    await browser.close()
+;(async () => {
+    const browser = await puppeteer.launch({ headless: false })
+    const page = await browser.newPage()
+    // await page.emulate(devices['iPhone 6'])
+    await page.setViewport({ width: 1920, height: 1080 })
+    await page.goto("http://fintech.lhjrc.com", { waitUntil: "networkidle2" })
+    await page.click("a[href='#/activity']")
+    let response = await page.waitForNavigation({ waitUntil: "networkidle2" })
+    console.log(page.url())
+
+    // await page.screenshot({path:'example.png',fullPage:true});
+    // await page.pdf({path: 'test.pdf', format: 'A4'});
+
+    // await browser.close()
 })()
