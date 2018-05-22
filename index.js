bea7f42fd2a92a482d2ba8437e5947ff5cc3aa73
Dean <mccarthey123@outlook.com>
2018-05-22 16:03:52 +0800

    screenshot 设置fullpage: true可以截取滚动屏幕的长图

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const puppeteer = require('puppeteer');
+const devices = require('puppeteer/DeviceDescriptors');
 
 (async ()=>{
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.emulate(devices['iPhone 6'])
     await page.goto('https://www.baidu.com');
-    await page.screenshot({path:'example.png'});
+    await page.screenshot({path:'example.png',fullPage:true});
     await browser.close()
 })()
