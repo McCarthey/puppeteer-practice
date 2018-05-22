8e9bb4a7602a9bd3d95a7293d79f5cbc5a7fcec1
Dean <mccarthey123@outlook.com>
2018-05-22 16:10:53 +0800

    测试生成pdf

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ const devices = require('puppeteer/DeviceDescriptors');
     await page.emulate(devices['iPhone 6'])
     await page.goto('https://www.baidu.com');
     await page.screenshot({path:'example.png',fullPage:true});
+    await page.pdf({path: 'test.pdf', format: 'A4'});
     await browser.close()
 })()
