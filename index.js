de54a3ba6e7e50ea5229843343cc356a3f0e4148
Dean <mccarthey123@outlook.com>
2018-05-23 16:02:25 +0800

    将截图移到generate目录下

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 const puppeteer = require("puppeteer")
 const devices = require("puppeteer/DeviceDescriptors")
 const delay = timeout => new Promise(resolve => setTimeout(resolve, timeout))
+const pageUrl = "https://www.humblebundle.com/books/web-design-development-books"
 
 ;(async () => {
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
-    // await page.emulate(devices['iPhone 6'])
-    await page.setViewport({ width: 1920, height: 1080 })
-    await page.goto("http://fintech.lhjrc.com", { waitUntil: "networkidle2" })
-
-    await page.click("a[href='#/news']")
-    await page.waitForNavigation({ waitUntil: "networkidle2" })
-    await delay(2000)
-    await page.click("a[href='#/policy']")
-    await page.waitForNavigation({ waitUntil: "networkidle2" })
-    await delay(2000)
-    await page.click("a[href='#/activity']")
-    await page.waitForNavigation({ waitUntil: "networkidle2" })
-    await delay(2000)
-    await page.click("a[href='#/log']")
-    await page.waitForNavigation({ waitUntil: "networkidle2" })
-    await delay(2000)
-    await page.click("a[href='#/sign']")
-    await page.waitForNavigation({ waitUntil: "networkidle2" })
-    await delay(2000)
-    console.log(page.url())
-
-    // await page.screenshot({path:'example.png',fullPage:true});
-    // await page.pdf({path: 'test.pdf', format: 'A4'});
-
-    // await browser.close()
+    await page.emulate(devices['iPhone 6'])
+    // await page.setViewport({ width: 1920, height: 1080 })
+    await page.goto(pageUrl,{waitUntil: "networkidle2"})
+    await page.screenshot({path:'./generate/hb.png',fullPage:true});
+    await browser.close()
 })()
