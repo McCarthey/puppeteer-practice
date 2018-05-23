6586dfe75461f5d0d3746ae13e53888f39f25c7f
Dean <mccarthey123@outlook.com>
2018-05-23 16:48:44 +0800

    升级成全局命令行工具

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+#!/usr/bin/env node
+
 const puppeteer = require("puppeteer")
 const devices = require("puppeteer/DeviceDescriptors")
-const delay = timeout => new Promise(resolve => setTimeout(resolve, timeout))
-const pageUrl = "https://www.humblebundle.com/books/web-design-development-books"
+const path = process.cwd()
 
-;(async () => {
+const pageUrl =
+    "https://www.baidu.com"
+
+function handleArgv(argv) {
+    console.log(argv)
+    if (argv[0] === "-h" || argv[0] === "--help") {
+        console.log("usage:\n")
+        console.log("-v or --version [show version]")
+        console.log("-s     or --shot [save a screenshot of given url]    ")
+    } else if (argv[0] === "-v" || argv[0] === "--version") {
+        console.log("v1.0.0")
+    } else if (argv[0] === "-s" || argv[0] === "--shot") {
+        screenshot()
+    }
+}
+async function screenshot() {
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
-    await page.emulate(devices['iPhone 6'])
+    await page.emulate(devices["iPhone 6"])
     // await page.setViewport({ width: 1920, height: 1080 })
-    await page.goto(pageUrl,{waitUntil: "networkidle2"})
-    await page.screenshot({path:'./generate/hb.png',fullPage:true});
+    await page.goto(pageUrl, { waitUntil: "networkidle2" })
+    await page.screenshot({ path: "./generate/hb.png", fullPage: true })
     await browser.close()
-})()
+}
+
+handleArgv(process.argv.slice(2))
