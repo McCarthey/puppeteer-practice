d2981498773c45e788cbe4e68cbc3abb15ff4199
Dean <mccarthey123@outlook.com>
2018-08-09 20:35:48 +0800

    基本每一个函数都应该await 否则会报错

--- prerender.js
+++ prerender.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 
 const puppeteer = require("puppeteer")
+const devices = require("puppeteer/DeviceDescriptors")
 
-const pageUrl = 'https://test.onluxy.com/static/landing_page/google_ad1.html#/'
+const pageUrl = 'https://www.loveblock.one'
 
+var document;
 
-async function prerender(device) {
+async function prerender(url) {
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
-    // await page.emulate(devices['iPhone 6'])
-    await page.setViewport({ width: 1920, height: 1080 })
-    const response = await page.goto(pageUrl, { waitUntil: "networkidle2" })
-    console.log(response)
-    await browser.close()
+    await page.emulate(devices['iPhone 6'])
+    // await page.setViewport({ width: 1920, height: 1080 })
+    await page.goto(url)
+    const html = await page.evaluate(() => `<html>${document.head.innerHTML}${document.body.innerHTML}</html>`);
+	await browser.close()
+	return html;
 }
 
-// handleArgv(process.argv.slice(2))
-prerender()
+prerender(pageUrl).then(html => console.log(html));
