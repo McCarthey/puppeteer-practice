c9e840f745a244c5c299aeb263144c4838e9791e
Dean <mccarthey123@outlook.com>
2018-08-14 15:31:44 +0800

    利用commander inquirer改造截图工具

--- index.js
+++ index.js
@@ -3,35 +3,41 @@
 const puppeteer = require("puppeteer")
 const devices = require("puppeteer/DeviceDescriptors")
 const path = process.cwd()
+const program = require('commander')
+const inquirer = require('inquirer')
 
-let pageUrl = 'http://localhost:39150/ddd.html#/airdrop'
+// let pageUrl = 'http://localhost:39150/ddd.html#/airdrop'
 
 function handleArgv(argv) {
-    console.log(argv)
-    if (argv[0] === "-h" || argv[0] === "--help") {
-        console.log("usage:\n")
-        console.log("-v or --version [show version]")
-        console.log("-s     or --shot [save a screenshot of given url]    ")
-    } else if (argv[0] === "-v" || argv[0] === "--version") {
-        console.log("v1.0.0")
-    } else if (argv[0] === "-s" || argv[0] === "--shot") {
-        
-        screenshot()
-    }
+    program
+        .version('0.0.1')
+        .option('-s')
+        .parse(process.argv)
+    console.log(program.list);
+
+    inquirer.prompt([{
+            type: 'input',
+            name: 'printUrl',
+            message: 'input url your want to print'
+        }])
+        .then(answers => {
+            console.log(answers.printUrl)
+            screenshot(answers.printUrl)
+        })
 }
-async function screenshot(device) {
+async function screenshot(url, device) {
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
     // await page.emulate(devices['iPhone 6'])
     await page.setViewport({ width: 1920, height: 1080 })
-    await page.goto(pageUrl, { waitUntil: "networkidle2" })
+    await page.goto(url, { waitUntil: "networkidle2" })
     await page.screenshot({
         path: "/puppeteer-practice/generate/hb.jpeg",
         quality: 60,
         fullPage: true
     })
+    console.log(`${url} is printed!`)
     await browser.close()
 }
 
-// handleArgv(process.argv.slice(2))
-screenshot()
+handleArgv(process.argv.slice(2))
