824ceeddc74547da7efd7228791189acfccc94c9
Dean <mccarthey123@outlook.com>
2018-12-06 20:44:24 +0800

    爬取方案上的wording

---
+++ crawler-test.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const puppeteer = require("puppeteer")
+const devices = require("puppeteer/DeviceDescriptors")
+
+const pageUrl = 'http://ddos.4g518.com/static/webprofile%E6%96%B9%E6%A1%88/index.html#g=1&p=profile_____'
+
+var document;
+
+async function main(url) {
+    const browser = await puppeteer.launch({ headless: false })
+    const page = await browser.newPage()
+	await page.goto(url)
+	// await page.waitFor(2000);
+	let iframe = await page.frames().find(f => f.name() === 'mainFrame');
+	let texts = await iframe.$('#u974_text')
+	const result = await iframe.evaluate(e => {
+		const res = []
+		let arr = e.childNodes
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i].innerText && arr[i].innerText !== '\n') {
+				res.push(arr[i].innerText)
+			}
+		}
+		return res
+	}, texts)
+	
+	console.log(result)
+	// 格式化爬取的数据
+	const title = result.shift() // 第一项是标题
+	const resultObj = {
+		title,
+		tags: result
+	}
+	
+	let writerStream = fs.createWriteStream(`${title}.json`);
+	writerStream.write(JSON.stringify(resultObj), 'UTF8');
+	writerStream.end();
+	
+	browser.close();
+}
+
+main(pageUrl)
--- prerender.js
+++ prerender.js
@@ -3,19 +3,20 @@
 const puppeteer = require("puppeteer")
 const devices = require("puppeteer/DeviceDescriptors")
 
-const pageUrl = 'http://localhost:39150/ddd.html#/airdrop'
+const pageUrl = 'https://github.com/'
 
 var document;
 
-async function prerender(url) {
+async function main(url) {
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
     // await page.emulate(devices['iPhone 6'])
-    await page.setViewport({ width: 1920, height: 1080 })
+    // await page.setViewport({ width: 1920, height: 1080 })
     await page.goto(url)
-    const html = await page.evaluate(() => `<html>${document.head.innerHTML}${document.body.innerHTML}</html>`);
-	await browser.close()
-	return html;
+    const cookies = await page.cookies()
+    // const html = await page.evaluate(() => `<html>${document.head.innerHTML}${document.body.innerHTML}</html>`);
+    await browser.close()
+    return cookies
 }
 
-prerender(pageUrl).then(html => console.log(html));
+main(pageUrl)
