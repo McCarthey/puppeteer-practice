81ac5a62b2c2d9df048cd987ded4076038f17ae9
Randall <randall@duomai.com>
2019-05-06 12:08:45 +0800

    [add] ssr and max window

--- autotest/at-spider.js
+++ autotest/at-spider.js
@@ -3,15 +3,17 @@ const puppeteer = require("puppeteer")
 const url = 'http://spider.nk8s.cn/';
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false })
+    const browser = await puppeteer.launch({ headless: false, args: ['--start-maximized'] }) // 窗口最大化
     const page = await browser.newPage()
     await page.setViewport({ width: 1920, height: 1080 })
     await page.goto(url, { waitUntil: "networkidle2" })
 
     await page.type('#username', 'admin', { delay: 100 })
     await page.type('#password', 'admin', { delay: 100 })
-    await page.click('.login-form-button', {delay: 500})
+    await page.click('.login-form-button', { delay: 500 })
 
     await page.waitFor(3000)
+    const html = await page.content() // ssr
+    console.log(html)
     await browser.close()
 })()
